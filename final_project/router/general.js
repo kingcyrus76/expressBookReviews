911851700129edac65e158d37bb402df7ef24e4d
Erastus Izuchukwu <kingcyrxs47@gmail.com>
2023-04-10 16:28:21 -0400

    Adding new lines of code to skeletal files
    Signed-off-by: Erastus Izuchukwu <kingcyrxs47@gmail.com>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  res.send(books[author])
+ 
+
 });
 
 // Get all books based on title
